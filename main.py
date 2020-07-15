df2003424fb6e95a23b4c8d0329e2741ce71c700
Joel Macht <joel.macht@student.kit.edu>
2020-07-15 17:39:12 +0000

    Generte accesibility map

--- main.py
+++ main.py
@@ -5,24 +5,27 @@ import common
 import visualize
 
 
-file_name = common.featureTypes[0]
-common.initial_data = geopandas.read_file(
-	"data/processed/{}.processed.geojson".format(file_name), 
-	driver="GeoJSON"
-)
-visualize.plot_map(
-	common.initial_data,
-	"state",
-	[0, 1, 2],
-	"RdBu",
-	"maps",
-	"initial_data"
-)
+# visualize.plot_map(
+# 	common.initial_data,
+# 	"state",
+# 	[0, 1, 2],
+# 	"RdBu",
+# 	"maps",
+# 	"initial_data"
+# )
 
-mask = initial_data["NUTZUNG_LEVEL1"]!="Verkehr"
+mask = common.initial_data["NUTZUNG_LEVEL1"]!="Verkehr"
 non_traffic_data = common.initial_data[mask].copy()
 non_traffic_data["accessibility"] = accessibility.get_accessibility(
 	non_traffic_data,
 	common.road_network
 )
-
+non_traffic_data.to_file(
+	"data/maps/accessibility.geojson",
+	driver="GeoJSON"
+)
+visualize.plot_accessibility(
+	non_traffic_data,
+	"maps",
+	"accessibility"
+)
