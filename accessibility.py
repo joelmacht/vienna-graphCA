0e165569c22c6ba158810465434314b2e49ad13d
Joel Macht <joel.macht@student.kit.edu>
2020-07-15 13:20:26 +0000

    Compute distance on dataframe

--- accessibility.py
+++ accessibility.py
@@ -4,27 +4,35 @@ import matplotlib.pyplot as plt
 import common
 
 
-def distance_to_road(parcel):
-	distances = road_network_subset["geometry"].distance(parcel["geometry"])
+def distance_to_road(parcel, road_network):
+	projected_road = road_network["geometry"].to_crs(epsg=3395)
+	projected_parcel = parcel["geometry"].to_crs(epsg=3395)
+	distances = projected_road.distance(projected_parcel.item())
 	return distances.min()
 
-def add_accessibility(parcels):
-	parcels["accessibility"] = parcels.apply(distance_to_road, axis=1)
+def add_accessibility(parcels, road_network):
+	parcels["accessibility"] = parcels.apply(distance_to_road, axis=1, args=road_network)
 
 if __name__ == "__main__":
 	initial_data = common.initial_data
 	road_network = common.road_network
-	parcels_subset = initial_data[initial_data["BEZ"]=="19"].copy()
+
+	mask = (initial_data["BEZ"]=="19") & (["NUTZUNG_LEVEL1"]!="Verkehr")
+	parcels_subset = initial_data[mask].copy()
 	
 	road_network_subset = road_network[road_network["BEZ"]=="19"].copy()
 
-	add_accessibility(parcels_subset)
+	print(distance_to_road(parcels_subset.iloc[[1]], road_network_subset))
+
+	# add_accessibility(parcels_subset, road_network_subset)
 
 	fig = plt.figure(figsize=(6, 6))
 	ax = fig.add_subplot()
 	plt.axis("off")
 
-	road_network_subset.plot(ax=ax, color="pink")
-	parcels_subset.plot(ax=ax, column="accessibility", cmap="hot")
+	road_network_subset.plot(ax=ax, color="black")
+	parcels_subset.iloc[[1]].plot(ax=ax, color="red")
+
+	# plt.savefig("images/debugging/roadsubset.pdf")
 
 	plt.savefig("images/debugging/distance.pdf")
