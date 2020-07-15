ff921f78c2d9f9213adc013d589b4bfa4f3160c9
Joel Macht <joel.macht@student.kit.edu>
2020-07-15 16:35:35 +0000

    Add computation of accessibility in main

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import geopandas
 
+import accessibility
 import common
 import visualize
 
 
 file_name = common.featureTypes[0]
-initial_data = geopandas.read_file(
+common.initial_data = geopandas.read_file(
 	"data/processed/{}.processed.geojson".format(file_name), 
 	driver="GeoJSON"
 )
 visualize.plot_map(
-	initial_data,
+	common.initial_data,
 	"state",
 	[0, 1, 2],
 	"RdBu",
 	"maps",
 	"initial_data"
 )
+
+mask = initial_data["NUTZUNG_LEVEL1"]!="Verkehr"
+non_traffic_data = common.initial_data[mask].copy()
+non_traffic_data["accessibility"] = accessibility.get_accessibility(
+	non_traffic_data,
+	common.road_network
+)
+
